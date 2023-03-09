1b3f6db53d4f4bd11d9fc218adb18ebf3b177722
ralbertazzi <ra@bendingspoons.com>
2023-03-09 18:32:41 +0100

    feat: add plugin

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+# 1.0.0 (2023-03-10)
+
+- First release, compatible with Poetry `1.4.0`
---
+++ poetry_cached_url_deps_plugin/plugin.py
@@ -0,0 +1,37 @@
+import functools
+import os
+import tempfile
+import urllib.parse
+from pathlib import Path
+
+from poetry.console.application import Application
+from poetry.core.packages.package import Package
+from poetry.plugins.application_plugin import ApplicationPlugin
+from poetry.puzzle.provider import Provider
+from poetry.utils.authenticator import Authenticator
+from poetry.utils.helpers import download_file, get_file_hash
+
+
+def mock_get_package_from_url(cls, url: str, session: Authenticator) -> Package:
+    file_name = os.path.basename(urllib.parse.urlparse(url).path)
+    with tempfile.TemporaryDirectory() as temp_dir:
+        dest = Path(temp_dir) / file_name
+        download_file(url, dest, session=session, chunk_size=1024 * 1024 * 10)
+        package = cls.get_package_from_file(dest)
+
+        package.files = [{"file": file_name, "hash": "sha256:" + get_file_hash(dest)}]
+
+    package._source_type = "url"
+    package._source_url = url
+
+    return package
+
+
+class PoetryCachedUrlDepsPlugin(ApplicationPlugin):
+    def __init__(self) -> None:
+        super().__init__()
+        self._session = Authenticator(cache_id="url")
+
+    def activate(self, application: Application) -> None:
+        mocked = classmethod(functools.partial(mock_get_package_from_url, session=self._session))
+        Provider.get_package_from_url = mocked  # type: ignore
